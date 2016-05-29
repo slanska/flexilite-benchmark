2b64b05461d68e3fe9015906c7f9f72c6c7444e1
slanska <slan_ska@yahoo.com>
2016-05-29 15:14:13 -0400

    fileSys: get list of files

--- services/fileSys/index.ts
+++ services/fileSys/index.ts
@@ -11,6 +11,7 @@ import config = require('./config');
 import qs = require('qs');
 import Promise = require('bluebird');
 import _ = require('lodash');
+import IFileStats = FileSys.IFileStats;
 
 var router = express.Router();
 
@@ -21,12 +22,12 @@ router.get('/filesys', (req:express.Request, res:express.Response, next)=>
 {
     var p = qs.parse(req.query).path || '';
     var fullPath = path.join(config.rootPath, p);
-    fs.readdir(fullPath, (err, data)=>
+    fs.readdir(fullPath, (err, fileNames)=>
     {
         if (!err)
         {
             var promises = [];
-            _.forEach(data, (n:string)=>
+            _.forEach(fileNames, (n:string)=>
             {
                 promises.push(new Promise((resolve, reject)=>
                 {
@@ -48,7 +49,20 @@ router.get('/filesys', (req:express.Request, res:express.Response, next)=>
             {
                 var d = {} as FileSys.IFileList;
                 d.path = p;
-                d.files = fileStats;
+                d.files = [];
+                _.forEach(fileStats, (item:fs.Stats, ii:number)=>
+                {
+                    let fStats = {} as IFileStats;
+                    fStats.created = item.ctime;
+                    fStats.name = fileNames[ii];
+                    fStats.directoryName = p;
+                    fStats.isDirectory = item.isDirectory();
+                    fStats.size = item.size;
+                    fStats.lastAccessed = item.atime;
+                    fStats.modified = item.mtime;
+                    d.files.push(fStats);
+                });
+
                 res.json(d);
             }).catch((err)=>
             {
